01f11fc2a47bb6d1dfcd7081807482cd83476d7e
phamduythe <thepham3603@gmail.com>
2024-03-21 20:19:50 +0700

    push moi truong

---
+++ doan_nhomE/database/migrations/2024_03_21_123504_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id')->unsigned();
+            $table->integer('manu_id')->references('id')->on('manufactures');
+            $table->string('product_name');
+            $table->integer('price');
+            $table->string('image');
+            $table->text('description');
+            $table->integer('quantity');
+            $table->tinyinteger('feature');
+            $table->integer('sale');
+            $table->integer('star');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
